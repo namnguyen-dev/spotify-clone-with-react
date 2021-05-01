c74ced6c06182bff666ebde4e4dc22e7313ac106
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-05-01 20:28:17 +0700

    set up Login functionality

---
+++ src/spotify.js
@@ -0,0 +1,20 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const redirectUri = 'http://localhost:3000/';
+
+const clientId = '3d664acc1132469d8d3f37e11509bab1';
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  '%20'
+)}&response_type=token&show_dialog=true`;
