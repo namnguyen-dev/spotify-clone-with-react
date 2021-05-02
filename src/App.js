a5cfcec11a5346bf460370e62135b3fc93e65e0c
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-05-02 11:43:52 +0700

    add useEffect hook to handle login functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-
 import './App.css';
-import Login from './Login'
+import Login from './Login';
+import { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
-  return (
-    <div className="app">
-     <Login/>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+  return <div className="app">{token ? <h1>Im logged in</h1> : <Login />}</div>;
 }
 
 export default App;
--- src/spotify.js
+++ src/spotify.js
@@ -13,6 +13,14 @@ const scopes = [
 
 export const getTokenFromUrl = () => {
   return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      var parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
 };
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
